725040c6c82465c5f7af58250565f0ac1ffd7fed
cloudRaine <sebastian@physics-principles.com>
2025-07-31 13:08:32 +0200

    controller reconciliation logic

--- internal/controller/pod_controller.go
+++ internal/controller/pod_controller.go
@@ -19,11 +19,18 @@ package controller
 import (
 	"context"
 
-	corev1 "k8s.io/api/core/v1"
+	apierrors "k8s.io/apimachinery/pkg/api/errors"
 	"k8s.io/apimachinery/pkg/runtime"
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 	logf "sigs.k8s.io/controller-runtime/pkg/log"
+
+	corev1 "k8s.io/api/core/v1"
+)
+
+const (
+	addPodNameLabelAnnotation = "physics-principles.com/add-pod-name-label"
+	podNameLabel              = "physics-principles.com/pod-name"
 )
 
 // PodReconciler reconciles a Pod object
@@ -32,23 +39,76 @@ type PodReconciler struct {
 	Scheme *runtime.Scheme
 }
 
-// +kubebuilder:rbac:groups=core,resources=pods,verbs=get;list;watch;create;update;patch;delete
-// +kubebuilder:rbac:groups=core,resources=pods/status,verbs=get;update;patch
-// +kubebuilder:rbac:groups=core,resources=pods/finalizers,verbs=update
+// +kubebuilder:rbac:groups=core.physics-principles.com,resources=pods,verbs=get;list;watch;create;update;patch;delete
+// +kubebuilder:rbac:groups=core.physics-principles.com,resources=pods/status,verbs=get;update;patch
+// +kubebuilder:rbac:groups=core.physics-principles.com,resources=pods/finalizers,verbs=update
 
-// Reconcile is part of the main kubernetes reconciliation loop which aims to
-// move the current state of the cluster closer to the desired state.
-// TODO(user): Modify the Reconcile function to compare the state specified by
-// the Pod object against the actual cluster state, and then
-// perform operations to make the cluster state reflect the state specified by
-// the user.
-//
-// For more details, check Reconcile and its Result here:
-// - https://pkg.go.dev/sigs.k8s.io/controller-runtime@v0.21.0/pkg/reconcile
+// Reconcile handles a reconciliation request for a Pod.
+// If the Pod has the addPodNameLabelAnnotation annotation, then Reconcile
+// will make sure the podNameLabel label is present with the correct value.
+// If the annotation is absent, then Reconcile will make sure the label is too.
 func (r *PodReconciler) Reconcile(ctx context.Context, req ctrl.Request) (ctrl.Result, error) {
-	_ = logf.FromContext(ctx)
+	log := logf.FromContext(ctx).WithValues("pod", req.NamespacedName)
 
-	// TODO(user): your logic here
+	/*
+		Step 0: Fetch the Pod from the Kubernetes API.
+	*/
+
+	var pod corev1.Pod
+	if err := r.Get(ctx, req.NamespacedName, &pod); err != nil {
+		if apierrors.IsNotFound(err) {
+			// we'll ignore not-found errors, since we can get them on deleted requests.
+			return ctrl.Result{}, nil
+		}
+		log.Error(err, "unable to fetch Pod")
+		return ctrl.Result{}, err
+	}
+
+	/*
+		Step 1: Add or remove the label.
+	*/
+
+	labelShouldBePresent := pod.Annotations[addPodNameLabelAnnotation] == "true"
+	labelIsPresent := pod.Labels[podNameLabel] == pod.Name
+
+	if labelShouldBePresent == labelIsPresent {
+		// The desired state and actual state of the Pod are the same.
+		// No further action is required by the operator at this moment.
+		log.Info("no update required")
+		return ctrl.Result{}, nil
+	}
+
+	if labelShouldBePresent {
+		// If the label should be set but is not, set it.
+		if pod.Labels == nil {
+			pod.Labels = make(map[string]string)
+		}
+		pod.Labels[podNameLabel] = pod.Name
+		log.Info("adding label")
+	} else {
+		// If the label should not be set but is, remove it.
+		delete(pod.Labels, podNameLabel)
+		log.Info("removing label")
+	}
+
+	/*
+		Step 2: Update the Pod in the Kubernetes API.
+	*/
+
+	if err := r.Update(ctx, &pod); err != nil {
+		if apierrors.IsConflict(err) {
+			// The Pod has been updated since we read it.
+			// Requeue the Pod to try to reconciliate again.
+			return ctrl.Result{Requeue: true}, nil
+		}
+		if apierrors.IsNotFound(err) {
+			// The Pod has been deleted since we read it.
+			// Requeue the Pod to try to reconciliate again.
+			return ctrl.Result{Requeue: true}, nil
+		}
+		log.Error(err, "unable to update Pod")
+		return ctrl.Result{}, err
+	}
 
 	return ctrl.Result{}, nil
 }
